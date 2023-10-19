a27668a5733b447bad33f2e6e60b4c7268102493
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-19 16:42:11 -0500

    frontend can make http request to backend

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const [name, setName] = React.useState("World");
+
+    useEffect(() => {
+        fetch("http://localhost:8000/api/")
+            .then((res) => {
+                return res.json();
+            }).then((data) => {
+                console.log(data);
+                setName(data.name);
+                setIsLoaded(true);
+            }).catch(() => {
+                setIsLoaded(true);
+        })
+    }, [])
+
     return (
-        <div>Hello World!</div>
+        <>
+        {isLoaded ? (
+            <h1>Hello {name}</h1>
+            ) : (
+            <h1>Loading...</h1>
+            )}
+        </>
     )
 };
 
