f41217330e92a0b62cebf043982c3ddd309d9dee
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-24 14:26:26 -0500

    jest working with module import statements

--- frontend/src/components/example.test.js
+++ frontend/src/components/example.test.js
@@ -1,5 +1,7 @@
+import add from "./example";
+
 describe("testing example component", () => {
   it("should add 1 and 1 to equal 2", () => {
-    expect(1).toEqual(1);
+    expect(add(1, 1)).toEqual(2);
   });
 });
