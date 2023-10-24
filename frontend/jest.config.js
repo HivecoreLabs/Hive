95124e8e395864f3012cff358343f9b46f0e9445
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-24 15:09:40 -0500

    config jest to work with react testing library

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["/frontend/**/*.{js,jsx}"],
   coverageProvider: "v8",
+  setupFilesAfterEnv: ["./src/jest.setup.js"],
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
   },
