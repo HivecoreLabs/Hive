e0782cc2f9c57d5a359c63c0c1e75d932cc2e285
steven cornwall <taylorchangordie@icloud.com>
2023-10-25 10:18:51 -0500

    updating quickstart to generate file and tab

--- backend/backend/quickstart.py
+++ backend/backend/quickstart.py
@@ -9,7 +9,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
@@ -58,6 +58,58 @@ def main():
     except HttpError as err:
         print(err)
 
+def generate(file_name):
+    """Generate a file using the sheets API based on the passed in file name
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        spreadsheet = service.spreadsheets().create(body={'properties': {'title': file_name}}).execute()
+
+        # Log the spreadsheet ID
+        spreadsheet_id = spreadsheet['spreadsheetId']
+        print(f"Created new spreadsheet with ID: {spreadsheet_id}")
+
+        body = {
+            "requests": [
+                {
+                    "addSheet": {
+                        "properties": {
+                            "title": 'testing'
+                        }
+                    }
+                },
+            ]
+        }
+
+        # Execute the request to create a new worksheet
+        response = service.spreadsheets().batchUpdate(
+        spreadsheetId=spreadsheet_id, body=body).execute()
+        return spreadsheet_id
+
+    except HttpError as err:
+        print(err)
+
 
 if __name__ == '__main__':
-    main()
+    # main()
+    generate("primary_database")
