1dd3f432928781563ba961ab361bdbdd7f51475b
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-24 19:07:24 -0500

    update jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["/frontend/**/*.{js,jsx}"],
-  coverageProvider: "v8",
   setupFilesAfterEnv: ["./src/jest.setup.js"],
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
