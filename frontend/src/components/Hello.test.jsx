4ed0bf60dd4ba45f73822884194e50f0f70894d7
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-24 19:58:23 -0500

    test

--- frontend/src/components/Hello.test.jsx
+++ frontend/src/components/Hello.test.jsx
@@ -5,8 +5,8 @@ import Hello from "./Hello";
 describe("test Hello Component", () => {
   it('should have the text "Hello World!"', () => {
     render(<Hello />);
-    const myElement = screen.getByText(/Hello World!/);
     screen.debug();
+    const myElement = screen.getByText(/Hello World!/);
     expect(myElement).toBeInTheDocument();
   });
 });
