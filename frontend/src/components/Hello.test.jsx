1d28988ea57fe85f8deab742257020370cd3027c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-24 19:20:16 -0500

    trying to get pre commit

--- frontend/src/components/Hello.test.jsx
+++ frontend/src/components/Hello.test.jsx
@@ -5,7 +5,7 @@ import Hello from "./Hello";
 describe("test Hello Component", () => {
   it('should have the text "Hello World!"', () => {
     render(<Hello />);
-
+    debugger;
     debugger;
     const myElement = screen.getByText(/Hello World!/);
     expect(myElement).toBeInTheDocument();
