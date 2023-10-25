8636451b52739150d47d45b91e435bfc936382a0
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-24 21:02:52 -0500

    check test

--- frontend/src/components/example.test.js
+++ frontend/src/components/example.test.js
@@ -1,7 +1,7 @@
-import add from "./example";
+import add from './example';
 
-describe("testing example component", () => {
-  it("should add 1 and 1 to equal 2", () => {
+describe('testing example component', () => {
+  it('should add 1 and 1 to equal 2', () => {
     expect(add(1, 1)).toBe(2);
   });
 });
