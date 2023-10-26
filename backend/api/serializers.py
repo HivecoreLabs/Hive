db2875d34dbfe7af423093acc0a42a3353fc5060
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-26 17:46:30 -0500

    add serializers for roles and employees

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Role
+from .models import Role, Employee
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,7 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RoleSerializer(serializers.ModelSerializer):
+    employees = serializers.PrimaryKeyRelatedField(
+        queryset=Employee.objects.all(), many=True)
+
     class Meta:
         model = Role
         fields = ['id', 'role', 'description', 'created_at',
                   'updated_at', 'sheet_cell', 'is_uploaded']
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    roles = RoleSerializer(many=True)
+
+    class Meta:
+        model = Employee
+        fields = ['first_name', 'last_name', 'restaurant_employee_id', 'roles', 'food_permit_exp',
+                  'alcohol_permit_exp', 'is_former_employee', 'created_at', 'updated_at', 'sheet_cell', 'is_uploaded']
