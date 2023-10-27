fd3e182a855c6f0f3499f957217534356d4e7c04
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-27 16:55:57 -0500

    update API docs to reflect requests and responses for roles

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import viewsets, status
 from rest_framework.authtoken.models import Token
 
 from django.contrib.auth.models import User
