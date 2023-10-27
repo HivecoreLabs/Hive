2fe2fe2eb77161c5de898183b70902bec3c431eb
nygilnet <nygilnet@gmail.com>
2023-10-27 00:10:47 -0500

    PROG: set up inputs and states for form inputs

--- frontend/src/components/EmployeeForm/index.jsx
+++ frontend/src/components/EmployeeForm/index.jsx
@@ -1,115 +1,151 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 
-export default function EmployeeForm() {
+export default function EmployeeForm({ employee, formType }) {
 
-    const testUsers = [
-        {
-            id: 1,
-            firstName: 'John',
-            lastName: 'Doe',
-            restaurantEmployeeId: 'EmpID22',
-            isUploaded: false,
-            foodPermitExp: 'Thur Oct 26 2023',
-            alcoholPermitExp: 'Thur Oct 26 2023',
-            roles: [
-                {
-                    role: 'Server'
-                }
-            ]
-        },
-        {
-            id: 2,
-            firstName: 'Jane',
-            lastName: 'Moore',
-            restaurantEmployeeId: 'EmpID10',
-            isUploaded: false,
-            foodPermitExp: 'Thur Oct 26 2023',
-            alcoholPermitExp: 'Thur Oct 26 2023',
-            roles: [
-                {
-                    role: 'Busser'
-                }
-            ]
-        },
-        {
-            id: 3,
-            firstName: 'Lenore',
-            lastName: 'Meyer',
-            restaurantEmployeeId: 'EmpID23',
-            isUploaded: false,
-            foodPermitExp: 'Thur Oct 26 2023',
-            alcoholPermitExp: 'Thur Oct 26 2023',
-            roles: [
-                {
-                    role: 'Chef'
-                },
-                {
-                    role: 'Carter'
-                }
-            ]
-        }
-    ];
+    // const testUsers = [
+    //     {
+    //         id: 1,
+    //         firstName: 'John',
+    //         lastName: 'Doe',
+    //         restaurantEmployeeId: 'EmpID22',
+    //         isUploaded: false,
+    //         foodPermitExp: 'Thur Oct 26 2023',
+    //         alcoholPermitExp: 'Thur Oct 26 2023',
+    //         roles: [
+    //             {
+    //                 role: 'Server'
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         id: 2,
+    //         firstName: 'Jane',
+    //         lastName: 'Moore',
+    //         restaurantEmployeeId: 'EmpID10',
+    //         isUploaded: false,
+    //         foodPermitExp: 'Thur Oct 26 2023',
+    //         alcoholPermitExp: 'Thur Oct 26 2023',
+    //         roles: [
+    //             {
+    //                 role: 'Busser'
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         id: 3,
+    //         firstName: 'Lenore',
+    //         lastName: 'Meyer',
+    //         restaurantEmployeeId: 'EmpID23',
+    //         isUploaded: false,
+    //         foodPermitExp: 'Thur Oct 26 2023',
+    //         alcoholPermitExp: 'Thur Oct 26 2023',
+    //         roles: [
+    //             {
+    //                 role: 'Chef'
+    //             },
+    //             {
+    //                 role: 'Carter'
+    //             }
+    //         ]
+    //     }
+    // ];
 
+    formType = 'create';
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [restaurantId, setRestaurantId] = useState('');
+    const [role, setRole] = useState([]);
+    const [foodPermitExp, setFoodPermitExp] = useState('');
+    const [alcoholPermitExp, setAlcoholPermitExp] = useState('');
+
+    const [validationErrors, setValidationErrors] = useState({
+
+    });
+    const [attempt, setAttempt] = useState(false);
+
+    const handleSubmit = async (e) => {
+
+
+
+    };
 
     return (
-        <>
-            <div className='employee-form'>
-                <div className='employee-form-header'>
-                    <h1>Employee Form</h1>
-                </div>
-                <div className='employee-form-employees'>
-                    <div className='employee-form-table-headers'>
-                        <div className='employee-form-first-name'>
-                            First Name
-                        </div>
-                        <div className='employee-form-last-name'>
-                            Last Name
-                        </div>
-                        <div className='employee-form-restaurant-id'>
-                            Restaurant ID
-                        </div>
-                        <div className='employee-form-role'>
-                            Role (select all that apply)
-                        </div>
-                        <div className='employee-form-food-permit-exp'>
-                            Food Permit Expiration
-                        </div>
-                        <div className='employee-form-alcohol-permit-exp'>
-                            Alcohol Permit Expiration
-                        </div>
-                    </div>
-                    {
-                        testUsers?.map(user => (
-                            <div className='employee-form-employee' id={user.id}>
-                                <div className='employees-first-name'>
-                                    {user.firstName}
-                                </div>
-                                <div className='employees-last-name'>
-                                    {user.lastName}
-                                </div>
-                                <div className='employees-restaurant-id'>
-                                    {user.restaurantEmployeeId}
-                                </div>
-                                <div className='employees-roles'>
-                                    space
-                                </div>
-                                <div className='employee-food-permit-exp'>
-                                    {user.foodPermitExp}
-                                </div>
-                                <div className='employee-alcohol-permit-exp'>
-                                    {user.alcoholPermitExp}
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-                <div className='employee-form-actions'>
-                    <button>Delete users</button>
-                    <button>Add new user</button>
-                </div>
+        <div className='employee-form-container'>
+            <div className='employee-form-header'>
+                {`${formType === 'create' ? 'Create': 'Edit'} Employee Form`}
             </div>
-        </>
+            <form 
+            className='employee-form'
+            onSubmit={handleSubmit}
+            >
+                <div className='employee-form-first-name'>
+                    <label>
+                        First Name
+                    </label>
+                    <input 
+                        type='text'
+                        id='first-name'
+                        // placeholder='First Name'
+                        value={firstName}
+                        onChange={e => setFirstName(e.target.value)}
+                    />
+                </div>
+                <div className='employee-form-last-name'>
+                    <label>
+                        Last Name
+                    </label>
+                    <input 
+                        type='text'
+                        id='last-name'
+                        // placeholder='Last Name'
+                        value={lastName}
+                        onChange={e => setLastName(e.target.value)}
+                    />
+                </div>
+                <div className='employee-form-restaurant-id'>
+                    <label>
+                        Restaurant ID
+                    </label>
+                    <input 
+                        type='text'
+                        id='restaurant-id'
+                        // placeholder='Restaurant ID'
+                        value={restaurantId}
+                        onChange={e => setRestaurantId(e.target.value)}
+                    />
+                    
+                </div>
+                <div className='employee-form-role'>
+                    <label>
+                        Role (select all that apply)
+                    </label>
+                </div>
+                <div className='employee-form-food-permit-exp'>
+                    <label>
+                        Food Permit Expiration
+                    </label>
+                    <input
+                        type='date'
+                        id='food-permit-exp'
+                        value={foodPermitExp}
+                        onChange={e => setFoodPermitExp(e.target.value)}
+                    />
+                </div>
+                <div className='employee-form-alcohol-permit-exp'>
+                    <label>
+                        Alcohol Permit Exipration
+                    </label>
+                    <input
+                        type='date'
+                        id='alcohol-permit-exp'
+                        value={alcoholPermitExp}
+                        onChange={e => setAlcoholPermitExp(e.target.value)}
+                    />
+                </div>
+            </form>
+        </div>
     );
 
 }
