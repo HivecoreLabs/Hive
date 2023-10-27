35068feee0e202882005f4ea93fef54a57523d90
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-27 13:33:43 -0500

    restructure test directory

--- backend/api/tests/urls/test_Role_urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from api.models import Role
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("auth/login/", views.login),
     path("auth/signup/", views.signup),
-    path("roles/", views.role_list),
+    path("roles/", views.role_list, name='role_list'),
     path('roles/<int:role_id>', views.role_detail),
     path('employees/', views.employee_list),
     path('employees/<int:employee_id>', views.employee_detail)
