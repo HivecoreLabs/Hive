3a3546b88e68ffa3d8119763c78c2f3a0f18ad9e
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-30 20:28:40 -0500

    add name for login and signup paths

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,7 @@ router.register('employees', views.EmployeeViewSet)
 router.register('roles', views.RoleViewSet)
 
 urlpatterns = [
-    path("auth/login/", views.login),
-    path("auth/signup/", views.signup),
+    path("auth/login/", views.login, name='login'),
+    path("auth/signup/", views.signup, name='signup'),
     path('', include(router.urls))
 ]
