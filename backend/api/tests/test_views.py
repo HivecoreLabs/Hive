6e01015cd2f94d23caebf220f2acbbbe03ab4e29
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-30 19:24:13 -0500

    add test for user login

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -15,7 +15,7 @@ class Test_Auth_Views(APITestCase):
         )
 
 
-    def test_new_user_signup(self) -> None:
+    def test_valid_new_user_signup(self) -> None:
         url = reverse('signup')
         response = self.client.post(url, {
             'username': 'Brandon',
@@ -23,8 +23,8 @@ class Test_Auth_Views(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         user_data = User.objects.get(username='Brandon')
+        user_token = Token.objects.get(user=user_data)
         serializer_data = UserSerializer(user_data).data
-        user_token = Token.objects.get(user_id=serializer_data['id'])
         expected_response = {
             'token': user_token.key,
             'user': {
@@ -45,8 +45,67 @@ class Test_Auth_Views(APITestCase):
         self.assertEqual(json.loads(response.content), {'username': ['A user with that username already exists.']})
 
 
-    def test_user_login(self) -> None:
-        pass
+    def test_valid_user_login(self) -> None:
+        signup_url = reverse('signup')
+        login_url = reverse('login')
+        self.client.post(signup_url, {
+            'username': 'Nygil',
+            'password': 'password'
+        })
+        response = self.client.post(login_url, {
+            'username': 'Nygil',
+            'password': 'password'
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        user = User.objects.get(username='Nygil')
+        serializer_data = UserSerializer(user).data
+        user_token = Token.objects.get(user=user).key
+        self.assertEqual(json.loads(response.content), {
+            'token': user_token,
+            'user': {
+                'id': serializer_data['id'],
+                'username': serializer_data['username'],
+            }
+        })
+
+
+    def test_invalid_credential_login(self):
+        # create user with hashed password
+        self.client.post(reverse('signup'), {
+            'username': 'Nygil',
+            'password': 'password'
+        })
+
+        url = reverse('login')
+        response = self.client.post(url, {})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(json.loads(response.content), {'username': ['This field is required.'], 'password': ['This field is required.']})
+
+        response = self.client.post(url, {'username': 'Nygil'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(json.loads(response.content), {'password': ['This field is required.']})
+
+        response = self.client.post(url, {'password': 'password'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(json.loads(response.content), {'username': ['This field is required.']})
+
+        response = self.client.post(url, {
+            'username': 'Nigil',
+            'password': 'password'
+        })
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(json.loads(response.content), {'detail': 'Not found.'})
+
+        response = self.client.post(url, {
+            'username': 'Nygil',
+            'password': 'password123'
+        })
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(json.loads(response.content), {'detail': 'Not found.'})
+
+
+
 
 class Test_Role_ViewSet:
     pass
