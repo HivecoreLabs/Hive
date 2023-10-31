728677ef59c8bb7f5d32e10139996d73655a9af0
Steven Taylor Cornwall <115431083+taylorcornwall766@users.noreply.github.com>
2023-10-31 10:25:50 -0500

    Update urls.py
    added /

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,6 +10,6 @@ router.register('roles', views.RoleViewSet)
 urlpatterns = [
     path("auth/login/", views.login),
     path("auth/signup/", views.signup),
-    path("spreadsheets", views.generate_sheet_database),
+    path("spreadsheets/", views.generate_sheet_database),
     path('', include(router.urls))
 ]
