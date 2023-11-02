5505ff77e2d29d0ec8d5c9f1a5b14f946bc4bb55
nygilnet <nygilnet@gmail.com>
2023-11-02 13:57:28 -0500

    PROG: updated employee page to have option to view all employees or create new employee

--- frontend/src/components/Employees/index.jsx
+++ frontend/src/components/Employees/index.jsx
@@ -1,45 +1,54 @@
+import "./index.css";
 import React from "react";
-import { Button } from "@mui/material";
-import { useEmployees } from "../../contexts/EmployeesContext";
-import { useRoles } from "../../contexts/RolesContext";
+import { css } from '@emotion/react';
+import { Box, Grid, Paper } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 function Employee() {
 
-    const { 
-        employee,
-        employees,
-        createEmployee,
-        readSingleEmployee,
-        readAllEmployees,
-        updateEmployee
-     } = useEmployees();
-
-     const {
-        role,
-        roles,
-        createRole,
-        readSingleRole,
-        readAllRoles,
-        updateRole
-     } = useRoles();
-
-    const test = (e) => {
-
-        e.preventDefault();
-
-        const res = readSingleEmployee(1);
-
-        console.log(res);
-
-    }
+    const gridStyle = css({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '100px',
+        marginLeft: '5px'
+    });
 
     return (
-        <>
-            <h1>Testing dispatch functions</h1>
-            <Button onClick={test}>
-                TEST
-            </Button>
-        </>
+        <div className="employee-page-container">
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Grid container rowSpacing={1} columnSpacing={2} sx={gridStyle}>
+                    <Grid item sx={{width: '300px'}}>
+                        <NavLink
+                        to="employees/all"
+                        >
+                            <Paper 
+                            elevation={2} 
+                            sx={{ borderRadius: 2, bgcolor: 'gray', display: 'flex', flexDirection: 'column', alignItems: 'center', textDecoration: 'none' }}
+                            >
+                                <p style={{ textDecoration: 'none' }}>
+                                    View/Edit Employees
+                                </p>
+                            </Paper>
+                        </NavLink>
+                    </Grid>
+                    <Grid item sx={{width: '300px'}}>
+                        <NavLink
+                        to="/employees/new"
+                        >
+                            <Paper 
+                            elevation={2} 
+                            sx={{ borderRadius: 2, bgcolor: 'gray', display: 'flex', flexDirection: 'column', alignItems: 'center', textDecoration: 'none' }}
+                            >
+                                <p style={{ textDecoration: 'none' }}>
+                                    Create New Employee
+                                </p>
+                            </Paper>
+                        </NavLink>
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
     );
 
 }
