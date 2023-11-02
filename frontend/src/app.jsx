ad42b89a53f20ca5208339d8b55941a89098a381
nygilnet <nygilnet@gmail.com>
2023-11-02 14:02:09 -0500

    PROG: added employee page and create employee form to frontend routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,8 +4,7 @@ import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Dashboard from './components/Dashboard';
 import Employee from './components/Employees/index.jsx';
-import EmployeeForm from './components/EmployeeForm/index.jsx';
-import SupportStaffPage from './components/SupportStaff';
+import CreateEmployeeForm from './components/EmployeeForm/CreateEmployee.jsx'
 import { useAuth } from './contexts/AuthenticationContext';
 import { theme } from './contexts/ThemeContext';
 
@@ -22,7 +21,7 @@ export default function App() {
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/dashboard" element={<Dashboard />} />
                 <Route exact path="/employees" element={<Employee />} />
-                <Route exact path="/support" element={<SupportStaffPage />} />
+                <Route path="/employees/new" element={<CreateEmployeeForm />} />
             </Routes>
         </>
     );
