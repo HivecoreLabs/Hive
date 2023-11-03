2e13da64853c8cbc9ca38a60e4c65730be617415
nygilnet <nygilnet@gmail.com>
2023-11-03 14:25:24 -0500

    PUSH: need employee ids to naviagate to edit employee form

--- frontend/src/components/ViewAllEmployees/index.jsx
+++ frontend/src/components/ViewAllEmployees/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useEmployees } from "../../contexts/EmployeesContext";
 
 function ViewAllEmployees() {
@@ -14,9 +15,25 @@ function ViewAllEmployees() {
 
     console.log(employees);
 
+    if (!employees[0]) return null;
     return (
         <div className='view-all-employees-container'>
             Hello from View All Employees Page!
+            <div className='view-all-employees-employees'>
+                <ul>
+                    {
+                        employees.map(e => (
+                            <NavLink to={`/employees/${e.id}`}>
+                                <li
+                                key={e.id}
+                                >
+                                    {`${e.last_name}, ${e.first_name}`}
+                                </li>
+                            </NavLink>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     );
 
