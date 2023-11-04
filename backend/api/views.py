e3075b410f30d5e22626157c13a8bc5b9c31b9b6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-03 21:33:55 -0500

    add date to seed data for clock ins and add query parameter for queryset in clock in view set

--- backend/api/views.py
+++ backend/api/views.py
@@ -240,7 +240,14 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
 
 class ClockInViewSet(viewsets.ModelViewSet):
-    queryset = Employee_Clock_In.objects.all()
+    def get_queryset(self):
+        queryset = Employee_Clock_In.objects.all()
+        params = self.request.query_params
+        date = params.get('date')
+        if date:
+            queryset = Employee_Clock_In.objects.filter(date=date)
+        return queryset
+
     def get_serializer_class(self, *args, **kwargs):
         if self.action == 'list' or self.action == 'retrieve':
             return Read_Clock_In_Serializer
