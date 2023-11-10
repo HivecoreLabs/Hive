992374d0af42a396d0d309e25c92a06abc175aa6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-09 21:47:18 -0600

    update calculate tipout received to round up to nearest whole dollar if not a flat rate

--- backend/api/utils/views/checkout_utils.py
+++ backend/api/utils/views/checkout_utils.py
@@ -1,4 +1,5 @@
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
+from math import ceil
 from sympy import sympify
 
 def group_by_active_role(employee_clockins):
@@ -9,6 +10,7 @@ def group_by_active_role(employee_clockins):
         else:
             res[clock_in.active_role_id].append(clock_in)
     return res
+
 
 def calculate_tipout_received_from_net_sales(formula, employee_list, net_sales):
     tipout_received = 0
@@ -22,8 +24,9 @@ def calculate_tipout_received_from_net_sales(formula, employee_list, net_sales):
     if max_tipout and is_time_based:
         tipout_received = min(max_tipout, calculated_tipout)
     elif max_tipout and not is_time_based:
-        # TODO: Figure out how max is calculated
         tipout_received = min(max_tipout * len(employee_list), calculated_tipout)
     else:
         tipout_received = calculated_tipout
-    return Decimal("{:.2f}".format(tipout_received))
+    tipout_received = Decimal(str(tipout_received))
+
+    return tipout_received.quantize(Decimal("1"), rounding=ROUND_HALF_UP) if expression.free_symbols else Decimal("{:.2f}".format(tipout_received))
